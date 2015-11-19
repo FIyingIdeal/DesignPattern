1cf39b7e2364e64d8ba8bd7a9b82e4a863404393
FlyingIdeal <FlyingIdeal_yan@163.com>
2015-11-19 20:38:09 +0800

    decorator pattern

---
+++ src/com/yanchao/designpatterns/decorator/Monkey.java
@@ -0,0 +1,13 @@
+package com.yanchao.designpatterns.decorator;
+
+/**
+ * Created by Administrator on 2015/11/19.
+ */
+public class Monkey implements TheGreatestSage{
+
+    @Override
+    public void currentShape() {
+        System.out.println("I'm a monkey");
+    }
+
+}
